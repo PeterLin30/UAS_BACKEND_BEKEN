c1047369d66816e67acef37cc1b39cfb7d98ab81
PeterLin30 <peterlin.030@gmail.com>
2026-06-19 23:58:30 +0700

    Hardcode JWT Secret secara mutlak

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const generateToken = (id, role) => {
-    return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    return jwt.sign({ id, role }, 'KunciRahasiaPeterLin2026', { expiresIn: '30d' });
 };
 
 const registerUser = async (req, res) => {
