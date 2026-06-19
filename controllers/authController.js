c3c6f59c4dfea15691e2b13966358f94f0150d84
PeterLin30 <peterlin.030@gmail.com>
2026-06-19 23:25:37 +0700

    revisi authcontroller.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,24 +7,24 @@ const generateToken = (id, role) => {
 };
 
 const registerUser = async (req, res) => {
-    const { name, email, password, role } = req.body;
-    const userExists = await User.findOne({ email });
+    try {
+        const { name, email, password, role } = req.body;
+        const userExists = await User.findOne({ email });
 
-    if (userExists) {
-        return res.status(400).json({ message: 'User already exists' });
-    }
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = await User.create({
-        name,
-        email,
-        password: hashedPassword,
-        role
-    });
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            role
+        });
 
-    if (user) {
         res.status(201).json({
             _id: user._id,
             name: user.name,
@@ -32,25 +32,29 @@ const registerUser = async (req, res) => {
             role: user.role,
             token: generateToken(user._id, user.role)
         });
-    } else {
-        res.status(400).json({ message: 'Invalid user data' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
 
 const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            role: user.role,
-            token: generateToken(user._id, user.role)
-        });
-    } else {
-        res.status(401).json({ message: 'Invalid credentials' });
+        if (user && (await bcrypt.compare(password, user.password))) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                token: generateToken(user._id, user.role)
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
 
