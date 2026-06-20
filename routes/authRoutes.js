e0618eb4ba6b94b2f8fc71bfe0301f207f94eb66
PeterLin30 <peterlin.030@gmail.com>
2026-06-20 12:07:14 +0700

    revisi

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+// Baris ini mutlak: Harus ada createAdmin di dalam kurung kurawal
 const { registerUser, loginUser, createAdmin } = require('../controllers/authController');
+const { protect, authorize } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
-module.exports = { registerUser, loginUser, createAdmin };
+// Rute pendaftaran admin
+router.post('/register-admin-first', createAdmin);
+
+module.exports = router;
