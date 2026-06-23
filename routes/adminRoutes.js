b0c5f95b0c25981fe444f20d66d1ffcb244bfa48
PeterLin30 <peterlin.030@gmail.com>
2026-06-23 23:05:25 +0700

    revisi

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const router = express.Router();
+
 const { 
     verifyCompany, 
     toggleUserStatus, 
     getAdminStats, 
     deleteJobByAdmin, 
     addCategory, 
-    getCategories 
+    getCategories,
+    getAllUsers 
 } = require('../controllers/adminController');
+
 const { protect, authorize } = require('../middleware/authMiddleware');
-const { getAdminStats, addCategory, getAllUsers } = require('../controllers/adminController'); 
-const { protect, admin } = require('../middleware/authMiddleware');
 
 router.put('/verify-company/:id', protect, authorize('admin'), verifyCompany);
 router.put('/toggle-user/:id', protect, authorize('admin'), toggleUserStatus);
 router.get('/stats', protect, authorize('admin'), getAdminStats);
 router.post('/categories', protect, authorize('admin'), addCategory);
 router.delete('/job/:id', protect, authorize('admin'), deleteJobByAdmin);
-
 router.get('/categories', getCategories);
-router.get('/user', protect, admin, getAllUsers);
+
+router.get('/users', protect, authorize('admin'), getAllUsers);
 
 module.exports = router;
